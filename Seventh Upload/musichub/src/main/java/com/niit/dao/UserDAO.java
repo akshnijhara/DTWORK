99158b1f9d374a92fb311c36ac2f66344bb345a3
akshnijhara <aksh_nijhara@Akshs-MacBook-Air.local>
2016-10-06 15:07:34 +0530

    Seventh Upload
    Registration Page implemented with the use of Spring Form

---
+++ Seventh Upload/musichub/src/main/java/com/niit/dao/UserDAO.java
@@ -0,0 +1,13 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Users;
+
+public interface UserDAO {
+	public void addUser(Users users);
+	public void deleteUser(Users users);
+	public Users getUserById(int userId);
+	public void editUser(Users user); 
+	public List<Users> getAllUsers();
+}
---
+++ Seventh Upload/musichub/src/main/java/com/niit/daoImpl/UserDAOImpl.java
@@ -0,0 +1,48 @@
+package com.niit.daoImpl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.niit.dao.UserDAO;
+import com.niit.model.Category;
+import com.niit.model.Users;
+
+public class UserDAOImpl implements UserDAO {
+	@Autowired
+private SessionFactory sessionFactory;
+	
+	public void addUser(Users users) {
+		sessionFactory.getCurrentSession().save(users);
+		
+	
+	}
+
+	@Override
+	public void deleteUser(Users users) {
+		sessionFactory.getCurrentSession().delete(users);
+			
+		
+	}
+
+	@Override
+	public Users getUserById(int userId) {
+		return	(Users) sessionFactory.getCurrentSession().get(Users.class, (Serializable) userId);
+		
+	}
+
+	@Override
+	public void editUser(Users users) {
+		
+		sessionFactory.getCurrentSession().update(users);
+		
+	}
+
+	@Override
+	public List<Users> getAllUsers() {
+		return	sessionFactory.getCurrentSession().createQuery("from Users").list();
+	}
+}
+
